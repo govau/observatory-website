dcb0fef2a9b37ed7c5c8fdca64b09219d4351f42
M-BG <37681628+M-BG@users.noreply.github.com>
2020-06-18 09:45:13 +1000

    grammar check to alert text

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -63,8 +63,8 @@ const BlogsPage: React.FC<PageContext> = ({ pageContext, location }) => {
             <>
               <h3>No upcoming events</h3>
               <p>
-                Due to COVID-19 we are rethinking how we can deliver training,
-                please signup to our mailing list to be notified of when
+                Due to COVID-19 we are rethinking how we can deliver training.
+                Please signup to our mailing list to be notified of when
                 training recommences.
               </p>
             </>
