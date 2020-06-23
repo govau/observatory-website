0c15ae8721f1218da41888a3bb8437c0417db134
Raj <sukhrajghuman@live.com>
2020-06-23 12:33:51 +1000

    Update blog-layout.tsx

--- src/components/layouts/blog-layout.tsx
+++ src/components/layouts/blog-layout.tsx
@@ -4,6 +4,8 @@ import DefaultLayout from "./default-layout";
 import SEO from "../seo";
 import { FormatDate } from "../helpers/helper";
 import SubscribeNewsletterForm from "../forms/newsletter/subscribe-newsletter";
+import Section from "./section";
+import SubscribeBlock from "../blocks/subscribe-newsletter-block";
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface Props {
   location: any;
@@ -18,33 +20,34 @@ const BlogLayout: React.FC<Props> = ({ pageContext, location, data }) => {
 
   return (
     <DefaultLayout pageContext={pageContext} location={location}>
-      <div className="au-body">
-        <SEO
-          title={frontmatter.title}
-          canonical={frontmatter.canonical}
-          description={frontmatter.metaDesc}
-        />
-        <div className="container-fluid">
-          <h1 className="blog-heading">{frontmatter.title}</h1>
-          <p>
-            {date} | {timeToRead} min read time
-          </p>
-          <p className="intro-text">{frontmatter.description}</p>
+      <>
+        <div className="au-body">
+          <SEO
+            title={frontmatter.title}
+            canonical={frontmatter.canonical}
+            description={frontmatter.metaDesc}
+          />
+          <div className="container-fluid">
+            <h1 className="blog-heading">{frontmatter.title}</h1>
+            <p>
+              {date} | {timeToRead} min read time
+            </p>
+            <p className="intro-text">{frontmatter.description}</p>
+          </div>
+          <div
+            className="container-fluid"
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></div>
         </div>
-        <div
-          className="container-fluid"
-          dangerouslySetInnerHTML={{ __html: html }}
-        ></div>
-        <div className="container-fluid">
-          <br />
-          <h3>Want the latest?</h3>
-          <p>
-            We regularly share what we are working on and things we learn in our
-            journey. Subscribe so you don&apos;t miss out our updates!
-          </p>
-          <SubscribeNewsletterForm />
-        </div>
-      </div>
+        <Section alt={true}>
+          <SubscribeBlock
+            heading="Want the latest?"
+            body="We regularly share what we are working on and things we learn in our
+            journey. Subscribe so you don't miss out our updates!"
+            darkTextInput={true}
+          />
+        </Section>
+      </>
     </DefaultLayout>
   );
 };
