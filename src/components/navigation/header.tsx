e61d42f7ca60b2ec1389d4697b7924679a5a1453
Tyler Christensen <98563383+dta-tyler@users.noreply.github.com>
2022-05-24 15:46:59 +1000

    Add decommission notice

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import { AUHeader, Brand } from "../helpers/auds";
+import { AUHeader, AuPageAlert, Brand } from "../helpers/auds";
 
 interface Props {
   siteTitle: string;
 }
 
+const PageAlert: any = AuPageAlert;
 const Header: React.FC<Props> = ({ siteTitle }) => {
   return (
     <>
       <AUHeader dark>
         <div className="container-fluid">
+          <div className="row">
+            <PageAlert as="warning" className={`decommission-banner`}>
+              <p>Observatory has been decommissioned and will be taken down from 19 June 2022. For further information please contact <a href="mailto:info@dta.gov.au">info@dta.gov.au</a>.</p>
+            </PageAlert>
+          </div>
           <div className="row">
             <div className="col-md-12">
               <Brand
